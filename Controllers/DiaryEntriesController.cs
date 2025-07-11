79e38a422f8a127ca8585163e632a56003f04c02
Yiwen Wang <wangyiwenau@gmail.com>
2025-07-12 09:58:29 +1000

    get method to get all diaryEntries

---
+++ Controllers/DiaryEntriesController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebDiaryAPI.Data;
+using WebDiaryAPI.Models;
+
+namespace WebDiaryAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DiaryEntriesController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        public DiaryEntriesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public IEnumerable<DiaryEntry> GetDiaryEntries()
+        {
+            return _context.DiaryEntries.ToList();
+        }
+    }
+    
+}
